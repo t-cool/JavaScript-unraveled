2d2c2ef0536344a1edc5276e5d22e2581ac87cb0
t-cool <hiroki802@hotmail.com>
2024-02-26 23:39:14 +0900

    tweak

--- chain_method.js
+++ chain_method.js
@@ -5,3 +5,10 @@ let result = [1,2,3,4,5]
 
 console.log(result);
 
+
+let sum = [1,2,3,4,5]
+    .filter(x => x % 2 != 0) // 奇数のみをフィルタリング
+    .map(x => x * 3) // 各要素を3倍にする
+    .reduce((acc, curr) => acc + curr, 0); // 合計を計算
+
+console.log(sum);
