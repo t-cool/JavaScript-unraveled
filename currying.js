b2a423b4da63510b9f55686cd9bc17f072686ffc
t-cool <hiroki802@hotmail.com>
2024-02-27 08:41:43 +0900

    tweak

--- currying.js
+++ currying.js
@@ -1,3 +1,18 @@
+function add1(num) {
+  return num + 1;
+}
+
+function add2(num) {
+  return num + 2;
+}
+
+// このような定義を抽象化できます
+function add(a) {
+  return function(b) {
+    return a + b;
+  };
+}
+
 // 2つの引数を取る関数をカリー化する
 function add(a) {
   return function(b) {
