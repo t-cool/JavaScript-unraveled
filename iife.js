a3f8c304f3e9e3b2201a417af6863f4558e55f2e
t-cool <hiroki802@hotmail.com>
2024-03-21 12:16:44 +0900

    tweak

---
+++ iife.js
@@ -0,0 +1,35 @@
+const Bird = (function() {
+    // プライベート変数と関数
+    let name = '';
+    let sound = '';
+
+    function setName(newName) {
+        name = newName;
+    }
+
+    function setSound(newSound) {
+        sound = newSound;
+    }
+
+    function getName() {
+        return name;
+    }
+
+    function getSound() {
+        return sound;
+    }
+
+    // 公開するメソッドとプロパティ
+    return {
+        setName: setName,
+        setSound: setSound,
+        getName: getName,
+        getSound: getSound
+    };
+})();
+
+// 使用例
+Bird.setName('スズメ');
+Bird.setSound('チュンチュン');
+console.log(Bird.getName()); // スズメ
+console.log(Bird.getSound()); // チュンチュン
