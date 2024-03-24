23806286a32a8f1d6797d07f75d8108d8de20f07
t-cool <hiroki802@hotmail.com>
2024-03-24 09:47:30 +0900

    tweak

--- design_patterns/strategy.js
+++ design_patterns/strategy.js
@@ -1,48 +1,40 @@
-// JavaScriptには他の言語のようなインターフェイスがないため、インターフェイスを模倣するためには異なるアプローチを取る必要があります。ストラテジーパターンを実装する一つの方法は、特定の関数を持つオブジェクト（ストラテジー）を定義し、それらを使用するコンテキストを作成することです。以下はその一例です。
-
 // ストラテジーインターフェイスを模倣する
-class Strategy {
-  execute() {
-    throw new Error('executeメソッドは実装されなければなりません。');
-  }
-}
+function Strategy() {}
+Strategy.prototype.execute = function() {
+  throw new Error('executeメソッドは実装されなければなりません。');
+};
 
 // 具体的なストラテジー1
-class ConcreteStrategyA extends Strategy {
-  execute() {
-    return 'ストラテジーAの実行';
-  }
-}
+function ConcreteStrategyA() {}
+ConcreteStrategyA.prototype = Object.create(Strategy.prototype);
+ConcreteStrategyA.prototype.execute = function() {
+  return 'ストラテジーAの実行';
+};
 
 // 具体的なストラテジー2
-class ConcreteStrategyB extends Strategy {
-  execute() {
-    return 'ストラテジーBの実行';
-  }
-}
+function ConcreteStrategyB() {}
+ConcreteStrategyB.prototype = Object.create(Strategy.prototype);
+ConcreteStrategyB.prototype.execute = function() {
+  return 'ストラテジーBの実行';
+};
 
 // コンテキストクラス
-class Context {
-  constructor(strategy) {
-    this.strategy = strategy;
-  }
-
-  setStrategy(strategy) {
-    this.strategy = strategy;
-  }
-
-  executeStrategy() {
-    return this.strategy.execute();
-  }
+function Context(strategy) {
+  this.strategy = strategy;
 }
+Context.prototype.setStrategy = function(strategy) {
+  this.strategy = strategy;
+};
+Context.prototype.executeStrategy = function() {
+  return this.strategy.execute();
+};
 
 // 使用例
-const strategyA = new ConcreteStrategyA();
-const strategyB = new ConcreteStrategyB();
-const context = new Context(strategyA);
+var strategyA = new ConcreteStrategyA();
+var strategyB = new ConcreteStrategyB();
+var context = new Context(strategyA);
 
 console.log(context.executeStrategy()); // ストラテジーAの実行
 
 context.setStrategy(strategyB);
 console.log(context.executeStrategy()); // ストラテジーBの実行
-
